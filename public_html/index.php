1871db5fd0a978850857cff10e5d73df423aea94
mironenkoff <mironenkoff@mail.ru>
2020-12-01 14:48:44 +0200

    Изменение путей к файлам

--- public_html/index.php
+++ public_html/index.php
@@ -1,12 +1,12 @@
 <?php
 
-require_once 'php/config.php';
+require_once '../php/config.php';
 //var_dump($dsn);
 //var_dump($user);
 //var_dump($password);
 
-require_once 'php/model.php';
-require_once 'php/controller.php';
+require_once '../php/model.php';
+require_once '../php/controller.php';
 
 $db = dbConnect( $dsn, $user, $password );
 //var_dump( $db );
