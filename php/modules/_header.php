74d3a207a4d0d409d5a311b43ebf5b9cc0cf2d1e
mironenkoff <mironenkoff@mail.ru>
2020-12-01 15:16:22 +0200

    Изменение названия ссылки

--- php/modules/_header.php
+++ php/modules/_header.php
@@ -5,7 +5,7 @@
   </a>
 
   <ul class="nav">
-    <li><a href="?action=toTS">Торговая стратегия</a></li>
+    <li><a href="?action=toTS">Торговая стратегия 2020</a></li>
     <li><a href="?action=toTradeLog">Журнал сделок</a></li>
     <li><a href="?action=toIMOEX">Структура IMOEX</a></li>
     <li><a href="?action=toStocks">Акции</a></li>
