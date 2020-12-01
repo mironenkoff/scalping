debf7f9ec3ab376d469be4c4bc6e722ddac33e88
mironenkoff <mironenkoff@mail.ru>
2020-12-01 15:28:53 +0200

    Эксперименты с путями

--- php/modules/_headTags.php
+++ php/modules/_headTags.php
@@ -1,6 +1,6 @@
 <title>MyTrading</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link href="../styles/reset.css" rel="stylesheet" type="text/css">
+<link href="../../styles/reset.css" rel="stylesheet" type="text/css">
 <link href="../styles/mystyle.css" rel="stylesheet" type="text/css">
 <link href="../fonts/fontawesome-free-5.15.1-web/css/fontawesome.css" rel="stylesheet" type="text/css">
