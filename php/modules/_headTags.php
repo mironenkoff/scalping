853f9ffebdfc0df96cb5df04e2e9a00516da9a13
mironenkoff <mironenkoff@mail.ru>
2020-12-01 15:44:32 +0200

    Переименовал файл со своими стилями

--- php/modules/_headTags.php
+++ php/modules/_headTags.php
@@ -2,5 +2,5 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link href="styles/reset.css" rel="stylesheet" type="text/css">
-<link href="styles/mystyle.css" rel="stylesheet" type="text/css">
+<link href="styles/myStyles.css" rel="stylesheet" type="text/css">
 <link href="fonts/fontawesome-free-5.15.1-web/css/fontawesome.css" rel="stylesheet" type="text/css">
